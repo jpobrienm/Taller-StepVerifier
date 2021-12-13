29931f60f5ac583bc19f71d5737e5a7801f1e9de
jpobrienm <33789137+jpobrienm@users.noreply.github.com>
2021-12-12 19:09:28 -0500

    se completa el taller de StepVerifier

---
+++ src/main/java/com/example/Spring/Webflux/Test/Tests/ServicioTest.java
@@ -0,0 +1,65 @@
+package com.example.Spring.Webflux.Test.Tests;
+
+import com.example.Spring.Webflux.Test.Services.Servicio;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@SpringBootTest
+class ServicioTest {
+    @Autowired
+    Servicio servicio;
+    @Test
+    void testMono() {
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno).expectNext("Pedro").verifyComplete();
+    }
+    @Test
+    void testVarios() {
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+    }
+
+    @Test
+    void testVariosLento() {
+        Flux<String> uno = servicio.buscarTodosLento();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1)).verifyComplete();
+    }
+    @Test
+    void testTodosFiltro() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        StepVerifier
+                .create(source)
+                .expectNext("JOHN")
+                .expectNextMatches(name -> name.startsWith("MA"))
+                .expectNext("CLOE", "CATE")
+                .expectComplete()
+                .verify();
+    }
+    @Test
+    void testTodosFiltroError() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        Flux<String> error = source.concatWith(
+                Mono.error(new IllegalArgumentException("Mensaje de Error"))
+        );
+        StepVerifier
+                .create(error)
+                .expectNextCount(4)
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
+                        throwable.getMessage().equals("Mensaje de Error")
+                ).verify();
+    }
+}
